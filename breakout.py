c580660aa3c835b0f86086402f26acda6007c9de
Linow <cloos.richard@gmail.com>
2021-09-19 12:23:10 +0200

    Add ball color on tray + some comments

--- breakout.py
+++ breakout.py
@@ -19,13 +19,19 @@ fps = 60
 bgColor = (202, 194, 193)
 BrickColor = (36, 156, 177)  # breakbale blue
 uBrickColor = (14, 14, 14)  # unbreakable black
+trayColor = (156, 178, 102)
 
-
+# BRICK VARIABLES
 # number of brick
 rows = 6
 cols = 5
 # border of brick
 border = 3
+
+# BALL VARIABLES
+# remaining balls
+balls = 2
+ballSpeed = 4
 
 # Same seed for every launched
 r.seed(1000)
@@ -79,10 +85,10 @@ class tray():
         self.y = height - 40
         self.rect = pygame.Rect(
             self.x, self.y, self.trayWidth, self.trayHeight)
-        self.speed = 5
+        self.speed = 8
 
     def printTray(self):
-        pygame.draw.rect(screen, BrickColor, self.rect)
+        pygame.draw.rect(screen, trayColor, self.rect)
 
     def move(self):
         # get key pressed
@@ -97,17 +103,75 @@ class tray():
             self.rect.x += self.speed
 
 
+class ball():
+    def __init__(self, ballSpeed):
+        self.width = 10
+        self.height = 10
+        self.x = (width - self.width) // 2
+        self.y = (height - 50)
+        self.speedx = ballSpeed
+        self.speedy = -ballSpeed
+
+    def print(self):
+        pygame.draw.circle(screen, trayColor, (self.x, self.y), self.width)
+
+    def move(self, ballSpeed):
+        global balls
+        self.x += self.speedx
+        self.y += self.speedy
+
+        # check for borders
+        # left and right borders
+        if self.x > width - self.width or self.x - self.width < 0:
+            self.speedx *= -1
+
+        # top border
+        if self.y - self.height < 0:
+            self.speedy *= -1
+
+        # bottom border (it trail touch ball)
+        if self.y > height - 50:
+            # check if trail touch
+
+            # if no trail
+            # get a new ball
+            if balls > 0:
+                # delete a ball
+                balls -= 1
+                # reset ball position
+                self.x = (width - self.width) // 2
+                self.y = (height - 50)
+                self.speedx = ballSpeed
+                self.speedy = -ballSpeed
+            else:
+                print("gameOver")
+
+
 bricksWall = wall()
 bricksWall.createBricks()
 playerTray = tray()
-print(bricksWall.bricks)
+# print(bricksWall.bricks)
+playerBall = ball(ballSpeed)
 
 while running:
+    # limit clock
     clock.tick(fps)
+    # print background
     screen.fill(bgColor)
+
+    # print Wall
     bricksWall.printWall()
+
+    # print Tray
     playerTray.printTray()
+
+    # print ball
+    playerBall.print()
+
+    # move
     playerTray.move()
+    playerBall.move(ballSpeed)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
