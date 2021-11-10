d128b1f958609751471325413f842f321dafe7f9
Linow <cloos.richard@gmail.com>
2021-11-10 13:03:42 +0100

    Ajout du jeu dans l'évènement

--- tensorflow/breakout.py
+++ tensorflow/breakout.py
@@ -12,11 +12,16 @@ running = True
 # ─── FUNCTIONS FOR USER INPUT ──────── #
 
 
-def event_to_action(eventlist):
-    global running
-    for event in eventlist:
-        if event.type == pygame.QUIT:
-            running = False
+def key_to_action(key, paddle, width):
+    # move left
+    if key[pygame.K_LEFT] and paddle.rect.left > 0:
+        return -1
+
+    # move right
+    elif key[pygame.K_RIGHT] and paddle.rect.right < width:
+        return 1
+    else:
+        return 0
 
 
 clock = pygame.time.Clock()
@@ -25,11 +30,23 @@ while running:
     # limit clock
     clock.tick(fps)
 
-    # get event of the user
+    # get quit event
     get_event = pygame.event.get()
-    event_to_action(get_event)
-    # action = # [...], get action
-    # env.step(action) # calculate game step
+    for event in get_event:
+        if event.type == pygame.QUIT:
+            running = False
+
+    # get pressed key
+    key = pygame.key.get_pressed()
+
+    # get the action
+    action = key_to_action(key, env.paddle, env.width)
+
+    env.step(action)
+
+    # move the ball
+    env.ball.move(env.paddle.rect, env.wall.bricks)
+
     env.render()  # make pygame render calls to window
     pygame.display.update()  # update window
 pygame.quit()
--- tensorflow/breakout_gym.py
+++ tensorflow/breakout_gym.py
@@ -105,16 +105,302 @@ class wall():
                              ((brick[3].x + border), (brick[3].y + border), self.brickWidth - 2*border, self.brickHeight - 2*border))
 
 
+class paddle():
+    def __init__(self):
+        self.paddleWidth = paddleWidth
+        self.paddleHeight = 10
+        self.x = (width - self.paddleWidth)/2  # init position
+        self.y = paddlePositionY
+        self.rect = pygame.Rect(
+            self.x, self.y, self.paddleWidth, self.paddleHeight)
+        self.speed = 8
+
+    def printPaddle(self):
+        pygame.draw.rect(screen, color3, self.rect)
+
+    def move(self):
+        # get key pressed
+        key = pygame.key.get_pressed()
+
+        # move left
+        if key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speed
+
+        # move right
+        if key[pygame.K_RIGHT] and self.rect.right < width:
+            self.rect.x += self.speed
+
+
+class ball():
+    def __init__(self, ballSpeed):
+        self.rad = 10
+        self.x = width // 2 - self.rad  # init position of the rectangle
+        self.y = ballPositionY          # init position of the rectangle
+        self.speedx = ballSpeed
+        self.speedy = -ballSpeed
+        self.rect = pygame.Rect(
+            self.x, self.y, 2*self.rad, 2*self.rad)
+        self.directionH = 1
+        self.angle = -1
+        self.newAngle = -1
+        self.ballSpeed = ballSpeed
+
+    def printBall(self):
+        pygame.draw.circle(screen, color3, (self.rect.x +
+                                            self.rad, self.rect.y + self.rad), self.rad)
+
+    def move(self, paddleRect, wallBricks):
+        global balls
+        global gameRunning
+
+        # Add speed every tick to the ball's coordinates
+        self.x += self.speedx
+        self.y += self.speedy
+
+        # Update the position of the ball
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        #-- Check for screen borders --#
+        # left and right borders
+        if self.rect.right > width or self.rect.left < 0:
+            self.speedx *= -1
+
+        # top border
+        elif self.rect.top < 0:
+            self.speedy *= -1
+
+        # bottom border
+        elif self.rect.bottom > height:
+            # reset ball position
+
+            self.x = width // 2 - self.rad
+            self.y = ballPositionY
+            self.rect.x = self.x
+            self.rect.y = self.y
+            self.speedx = self.ballSpeed
+            self.speedy = -self.ballSpeed
+
+            # reset paddle position
+            paddleRect.x = (width - paddleWidth)/2  # init position
+
+            # get a new ball if availabe
+            if balls > 0:
+                gameRunning = 0
+                # delete a ball
+                balls -= 1
+
+            else:  # no ball left
+                global gameover
+                gameover = 1
+
+        #-- End Check for screen borders --#
+
+        #-- Check for collisions between ball and paddle --#
+        elif self.rect.y > paddlePositionY - 2*self.rad:
+            if self.rect.colliderect(paddleRect):
+
+                # TOP COLLISION
+                # check if ball is on top of the trail (between the 5px margin)
+                if(abs(self.rect.bottom - paddlePositionY) < margin and self.speedy > 0):
+
+                    # calculate angle between the ball's path and the trail
+                    if self.speedx == 0:
+                        self.angle = 90
+                    else:
+                        self.angle = abs(math.degrees(
+                            math.atan(self.speedy/abs(self.speedx))))
+
+                    if(self.speedx > 0):
+                        self.directionH = 1  # from the left
+                    else:
+                        self.directionH = -1  # from the right
+
+                    # change x if not on the middle of the paddle
+
+                    # LEFT PART
+                    if self.rect.right >= paddleRect.x and self.rect.left < paddleRect.x + (0.2 * paddleWidth):
+
+                        if self.directionH == 1:  # from the left
+                            # increase angle by 35%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*1.35
+
+                            elif self.angle > 60:  # increase angle by 25%
+                                self.newAngle = self.angle*1.25
+
+                            else:  # increase angle by 30%
+                                self.newAngle = self.angle*1.30
+
+                        else:  # from the right
+                            # reduce angle by 35%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*0.65
+
+                            elif self.angle > 60:  # reduce angle by 25%
+                                self.newAngle = self.angle*0.75
+
+                            else:  # reduce angle by 30%
+                                self.newAngle = self.angle*0.70
+
+                        self.speedx = - \
+                            (math.cos(math.radians(self.newAngle)) * velocity)
+                        self.speedy = - \
+                            (math.sin(math.radians(self.newAngle)) * velocity)
+
+                    # MIDDLE LEFT PART
+                    elif self.rect.right >= paddleRect.x + (0.2 * paddleWidth) and self.rect.left < paddleRect.x + (0.4 * paddleWidth):
+
+                        if self.directionH == 1:  # from the left
+                            # increase angle by 20%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*1.2
+
+                            elif self.angle > 60:  # increase angle by 10%
+                                self.newAngle = self.angle*1.1
+
+                            else:  # increase angle by 15%
+                                self.newAngle = self.angle*1.15
+
+                        else:  # from the right
+                            # reduce angle by 20%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*0.8
+
+                            elif self.angle > 60:  # reduce angle by 10%
+                                self.newAngle = self.angle*0.9
+
+                            else:  # reduce angle by 15%
+                                self.newAngle = self.angle*0.85
+
+                        self.speedx = - \
+                            (math.cos(math.radians(self.newAngle)) * velocity)
+                        self.speedy = - \
+                            (math.sin(math.radians(self.newAngle)) * velocity)
+
+                    elif self.rect.right >= paddleRect.x + (0.4 * paddleWidth) and self.rect.left < paddleRect.x + (0.6 * paddleWidth):
+                        # angle is not changed
+                        self.speedy *= -1
+
+                    # MIDLE RIGHT PART
+                    elif self.rect.right >= paddleRect.x + (0.6 * paddleWidth) and self.rect.left < paddleRect.x + (0.8 * paddleWidth):
+
+                        if self.directionH == 1:  # from the left
+                            # reduce angle by 20%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*0.8
+
+                            elif self.angle > 60:  # reduce angle by 10%
+                                self.newAngle = self.angle*0.9
+
+                            else:  # send the ball in opposite direction
+                                # reduce angle by 15%
+                                self.newAngle = self.angle*0.85
+
+                        else:  # from the right
+                            if self.angle < 30:
+                                # increase angle by 20%
+                                self.newAngle = self.angle*1.2
+
+                            elif self.angle > 60:  # increase angle by 10%
+                                self.newAngle = self.angle*1.1
+
+                            else:  # increase angle by 15%
+                                self.newAngle = self.angle*1.15
+
+                        self.speedx = \
+                            (math.cos(math.radians(self.newAngle)) * velocity)
+                        self.speedy = - \
+                            (math.sin(math.radians(self.newAngle)) * velocity)
+
+                    # RIGHT PART
+                    elif self.rect.right >= paddleRect.x + (0.8 * paddleWidth) and self.rect.left < paddleRect.x + paddleWidth:
+                        if self.directionH == 1:  # from the left
+                            # reduce angle by 35%
+                            if self.angle < 30:
+                                self.newAngle = self.angle*0.65
+
+                            elif self.angle > 60:  # reduce angle by 25%
+                                self.newAngle = self.angle*0.75
+
+                            else:  # send the ball in opposite direction
+                                # reduce angle by 30%
+                                self.newAngle = self.angle*0.70
+
+                        else:  # from the right
+                            if self.angle < 30:
+                                # increase angle by 35%
+                                self.newAngle = self.angle*1.35
+
+                            elif self.angle > 60:  # increase angle by 25%
+                                self.newAngle = self.angle*1.25
+
+                            else:  # increase angle by 30%
+                                self.newAngle = self.angle*1.30
+
+                        self.speedx = \
+                            (math.cos(math.radians(self.newAngle)) * velocity)
+                        self.speedy = - \
+                            (math.sin(math.radians(self.newAngle)) * velocity)
+
+                # SIDES COLLISIONS
+                else:  # collision with side
+                    if (abs(self.rect.left - paddleRect.right) < margin):
+                        # check direction of the ball
+                        if self.speedx < 0:
+                            self.speedx *= -1
+                        else:  # if same direction as the paddle don't reverse direction but increase speed
+                            self.speedx += 3
+
+                    elif (abs(self.rect.right - paddleRect.left) < margin):
+                        # check direction of the ball
+                        if self.speedx > 0:
+                            self.speedx *= -1
+                        else:  # if same direction as the paddle don't reverse direction but increase speed
+                            self.speedx -= 3
+
+        #-- End Check for collisions between ball and paddle --#
+
+        #-- Check for collisions between ball and Bricks --#
+        elif self.rect.y > 0 and self.rect.y < (height // 2) + 100:
+            for brick in wallBricks:
+                if(self.rect.colliderect(brick[3])):
+                    # check if collision is on top or at bottom of the brick
+                    if ((abs(self.rect.top - brick[3].bottom) < margin and self.speedy < 0) or
+                            (abs(self.rect.bottom - brick[3].top) < margin and self.speedy > 0)):
+                        # top or bottom
+                        self.speedy *= -1
+
+                    elif ((abs(self.rect.left - brick[3].right) < margin and self.speedx < 0) or
+                          (abs(self.rect.right - brick[3].left) < margin and self.speedx > 0)):
+                        # right or left
+                        self.speedx *= -1
+
+                    # delete the brick if breakable
+                    if(brick[2] != 0):
+                        wallBricks.remove(brick)
+
+        #-- End Check for collisions between ball and Bricks --#
+
+    def gravity(self):
+        if (self.speedy >= 0 and self.speedy < 1):  # stuck horizontally
+            self.speedy = 1
+        elif (self.speedy < 0 and self.speedy > -1):  # stuck horizontally
+            self.speedy = -1
+
+
 class BreakoutEnv(gym.Env):
     def __init__(self, env_config={}):
         # self.parse_env_config(env_config)
+        self.width = width
         self.screen = None
         self.action_space = Discrete(2)
         self.observation_space = Box(0, 255, [600, 640, 3])
 
         self.env = Environment(self)
         self.wall = wall()
-        # self.rocket = Rocket(self, self.env)
+        self.paddle = paddle()
+        self.ball = ball(ballSpeed)
         self.spectator = None
 
         self.reset()
@@ -122,17 +408,22 @@ class BreakoutEnv(gym.Env):
 
     def reset(self):
         # reset the environment to initial state
-        print("coucou")
+        print("reset")
 
         return "observation"
 
     def step(self, action):
         # perform one step in the game logic
+        if action == 1:
+            self.paddle.rect.x += self.paddle.speed
+        elif action == -1:
+            self.paddle.rect.x -= self.paddle.speed
 
         return "observation, reward, done, info"
 
     def render(self):
 
+        # done only once
         def init_pygame(self):
             pygame.init()
             self.screen = pygame.display.set_mode((width, height))
@@ -140,12 +431,18 @@ class BreakoutEnv(gym.Env):
             # create bricks walls
             self.wall.createBricks()
 
-        def render_wall(self):
+        def render(self):
+            # render wall
             self.wall.printWall()
+            # render paddle
+            self.paddle.printPaddle()
+            # render ball
+            self.ball.printBall()
 
+        # init pygame
         if self.screen is None:
             init_pygame(self)
 
         # print background
         self.screen.fill(color4)
-        render_wall(self)
+        render(self)
